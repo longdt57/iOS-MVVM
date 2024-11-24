3a5570264ecac52cfba22db1ec454d8e1af785e9
Long Do <Longdt57@gmail.com>
2024-11-24 20:39:35 +0900

    Init Project

---
+++ PureClean AI/PureClean_AIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PureClean_AIApp.swift
+//  PureClean AI
+//
+//  Created by Long Do on 22/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct PureClean_AIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
